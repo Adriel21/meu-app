396dee54ff2dbb9d3569e506dc886552387d8a7a
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-10 11:58:59 -0300

    css externo e notações

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,12 +1,18 @@
 import Menu from "../Menu";
 
+// objeto de estilos css
+const estilos = {
+  color: "white",
+  backgroundColor: "black", 
+};
+
 const Cabecalho = () => {
     return(
-      // CSS IN JS
+      // CSS IN JS embutidos no meo do JavaScript
 
       // CSS Inline (Uma propriedade style e sintaxe de objetos)
         <header style={{backgroundColor: "lightblue"}}>
-        <h1>Interface React</h1>
+        <h1 style={estilos}>Interface React</h1>
         <Menu/>
       </header>
     )
