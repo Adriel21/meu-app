91a99c9f2dbdf51b5c9723bed4ac382f99eb5269
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-10 09:42:22 -0300

    Mais alterações de reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+// As funções que vão funcionar como componentes e, arquivos também, devem começar com letra maíuscula
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
