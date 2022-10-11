84d6bccd254f5503464304e657278de09fd2c078
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-11 09:24:38 -0300

    Aplicando CSS modularizado

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,13 +1,17 @@
 import Artigo from "../Artigo";
-import './Conteudo.css';
+import estilos from './Conteudo.module.css';
 
 const Conteudo = () => {
 return (
     <main>
     <section>
       {/* comentário no jsx */}
-      <h2 className="subtitulo">Conteúdo do site</h2>
+      {/* Aplicando css com module.css */}
+      <h2 className={estilos.subtitulo}>Conteúdo do site</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus sed ab hic modi corporis obcaecati totam sint omnis illo soluta.</p>
+      <span className={`${estilos.destaque}  ${estilos["sombra-texto"]}`}>
+        texto
+      </span>
 
      <Artigo/>
 
