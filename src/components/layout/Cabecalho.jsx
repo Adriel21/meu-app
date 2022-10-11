a2534093e4caaae6c91cc96390a1edd7e96dc988
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-11 11:28:39 -0300

    Estilizando com styled-components

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,11 +1,38 @@
+import styled from "styled-components";
 import Menu from "../Menu";
+
+const periodo = "diurno";
+
+// Tagged Template - utilizar crases combinadas com funções - para uso com styled components: ``
+const StyledCabecalho = styled.header`
+background-color: ${periodo === 'diurno'? "lightblue" : "darkblue"};
+text-align: center;
+display: flex;
+justify-content: space-around;
+align-items: center;
+
+h1{
+  color: red;
+}
+
+p{
+  font-weight: bold;
+  color: blue;
+
+  &:hover {
+    color: red;
+    cursor: pointer;
+  }
+}
+`;
 
 const Cabecalho = () => {
     return(
-        <header>
+        <StyledCabecalho>
         <h1>Interface React</h1>
         <Menu/>
-      </header>
+        <p>Teste</p>
+      </StyledCabecalho>
     )
 };
 
