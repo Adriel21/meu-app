5b58ab7d2d986aa9272ce5e80975acbeb3e2d424
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-17 22:28:24 -0300

    Finalizado componentes utilizando props

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Cabecalho />
-      <Conteudo />
+      <Conteudo/>
       <Rodape />
     </>
   );
--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,23 +1,46 @@
 import Artigo from "../Artigo";
 
 const Conteudo = () => {
-    // 1) Crie um array com o nome de 3 cursos
+  /* 1) Crie um array com o nome de 3 cursos */
+  const cursos = ["JS Avançado", "React", "Node.js"];
 
-    const cursos = ["JS Avançado", "React", "Node.Js"];
+  /* 2) Dentro das chamadas dos artigos, passe como uma nova prop chamada curso
+  o nome de cada curso. O primeiro artigo deve mostrar o nome do primeiro curso, o segundo artigo o nome do segundo curso e o terceiro o nome do terceiro curso. */
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo do site....</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ab
+          inventore, sapiente impedit numquam maxime quia voluptates, et at modi
+          harum recusandae? Inventore illo consectetur, quisquam distinctio
+          commodi voluptate fugit.
+        </p>
 
-    // 2) Dentro das chamadas dos artigos passe como uma nova prop chamada curso o nome de cada curso. O primeiro artigo deve mostrar o nome do primeiro curso, o segundo artigo o nome do segundo curso e o terceiro o nome do terceiro curso
+        <Artigo
+          curso={cursos[0]}
+          titulo="Artigo 1"
+          subtitulo="Subtítulo do artigo 1"
+        >
+          Este é o texto do primeiro artigo....
+        </Artigo>
+        <Artigo
+          curso={cursos[1]}
+          subtitulo="Subtítulo do artigo 2"
+          titulo="Artigo 2"
+        >
+          Este é o texto do segundo artigo....
+        </Artigo>
+        <Artigo
+          curso={cursos[2]}
+          titulo="Artigo 3"
+          subtitulo="Subtítulo do artigo 3"
+        >
+          Este é o texto do terceiro artigo....
+        </Artigo>
+      </section>
+    </main>
+  );
+};
 
-    return(
-        <main>
-            <section>
-                Conteúdo do site...
-                <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ab
-                 inventore, sapiente impedit numquam maxime quia voluptates, et at modi
-                harum recusandae? Inventore illo consectetur, quisquam distinctio
-             commodi voluptate fugit.
-                </p>
-            </section>
-        </main>
-    )
-}
+export default Conteudo;
