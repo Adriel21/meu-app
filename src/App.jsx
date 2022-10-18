87a822561a2f4fb5f01cf63f2872375b8e29d929
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-18 13:11:25 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Cabecalho from "./components/layout/Cabecalho";
+import Rodape from "./components/layout/Rodape";
+import Home from "./pages/Home";
+import Produtos from "./pages/Produtos";
+import Servicos from "./pages/Servicos";
 
-// As funções que vão funcionar como componentes e, arquivos também, devem começar com letra maíuscula
-function App() {
-  
-}
+const App = () => {
+  return (
+    <>
+      {/* BrowserRouter: container de rotas 
+    (precisa envolver todos os componentes do App) */}
+      <BrowserRouter>
+        <Cabecalho />
+
+        {/* Switch: mecanismo para troca/alternância de rotas */}
+        <Switch>
+          {/* Route: configuração de cada rota 
+          (qual caminho, qual componente) */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/produtos">
+            <Produtos />
+          </Route>
+          <Route path="/servicos">
+            <Servicos />
+          </Route>
+        </Switch>
+
+        <Rodape />
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
